5f4e964d18912f0540b7c432a0718ec3d97f43ff
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-02-01 14:37:33 +0100

    Work on setup for Lstar

---
+++ src/main/java/nl/tudelft/instrumentation/learning/LearningLab.java
@@ -0,0 +1,72 @@
+package nl.tudelft.instrumentation.learning;
+
+import java.util.*;
+import java.util.Random;
+
+/**
+ * You should write your own solution using this class.
+ */
+public class LearningLab {
+        static Random r = new Random();
+        static List<String> currentTrace;
+        static int traceLength = 10;
+        static boolean isFinished = false;
+
+        static void initialize(String[] inputSymbols){
+                // Initialise a random trace from the input symbols of the problem.
+                currentTrace = generateRandomTrace(inputSymbols);
+        }
+
+        /**
+         * Method for fuzzing new inputs for a program.
+         * @param inputSymbols the inputSymbols to fuzz from.
+         * @return a fuzzed sequence
+         */
+        static List<String> fuzz(String[] inputSymbols){
+                /*
+                 * Add here your code for fuzzing a new sequence for the RERS problem.
+                 * You can guide your fuzzer to fuzz "smart" input sequences to cover
+                 * more branches. Right now we just generate a complete random sequence
+                 * using the given input symbols. Please change it to your own code.
+                 */
+                return generateRandomTrace(inputSymbols);
+        }
+
+        /**
+         * Generate a random trace from an array of symbols.
+         * @param symbols the symbols from which a trace should be generated from.
+         * @return a random trace that is generated from the given symbols.
+         */
+        static List<String> generateRandomTrace(String[] symbols) {
+                ArrayList<String> trace = new ArrayList<>();
+                for (int i = 0; i < traceLength; i++) {
+                        trace.add(symbols[r.nextInt(symbols.length)]);
+                }
+                return trace;
+        }
+
+        static void run() {
+                initialize(LearningTracker.inputSymbols);
+                LearningTracker.runNextFuzzedSequence(currentTrace.toArray(new String[0]));
+
+                // Place here your code to guide your fuzzer with its search.
+                while(!isFinished) {
+                        // Do things!
+                        try {
+                                System.out.println("Woohoo, looping!");
+                                Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                                e.printStackTrace();
+                        }
+                }
+        }
+
+        /**
+         * Method that is used for catching the output from standard out.
+         * You should write your own logic here.
+         * @param out the string that has been outputted in the standard out.
+         */
+        public static void output(String out){
+                System.out.println(out);
+        }
+}
---
+++ src/main/java/nl/tudelft/instrumentation/learning/LearningTracker.java
@@ -0,0 +1,85 @@
+package nl.tudelft.instrumentation.learning;
+import nl.tudelft.instrumentation.runner.CallableTraceRunner;
+
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.*;
+/**
+ * This class is used to start and run the tests for the algorithm that you
+ * will build to do automated code patching.
+ *
+ * @author Clinton Cao, Sicco Verwer
+ */
+public class LearningTracker {
+    static String[] inputSymbols;
+    static String currentOutput = "";
+    static int current_index = 0;
+    static List<String> outputs = new ArrayList<>();
+    static ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+    static CallableTraceRunner<Void> problem;
+
+    // Longest a single testcase is allowed to run
+    static final int timeoutMS = 1000;
+
+
+    /**
+     * Append the output of the program to a list containing all outputs.
+     * @param out
+     */
+    public static void output(String out){
+        LearningLab.output(out);
+        currentOutput = currentOutput + out;
+    }
+
+    /**
+     * Initialize some of the fields in this class.
+     * @param o the list of operators.
+     */
+    public static void initialize(String[] o){
+    }
+
+    /**
+     * Initialize and hand over control to PatchingLab
+     * @param inputSymbols The input symbols of the problem
+     * @param eca The current problem instance
+     */
+    public static void run(String[] inputSymbols, CallableTraceRunner<Void> eca) {
+        problem = eca;
+        LearningTracker.inputSymbols = inputSymbols;
+        LearningLab.run();
+    }
+
+    /**
+`     * This method is used for running the fuzzed input. It first assigns the
+     * fuzzed sequence that needs to be run and then user a handler to
+     * start running the sequence through the problem.
+     * @param sequence the fuzzed sequence that needs top be run.
+     */
+    public static void runNextFuzzedSequence(String[] sequence) {
+        problem.setSequence(sequence);
+        final Future handler = executor.submit(problem);
+        executor.schedule(() -> {
+            handler.cancel(true);
+        }, timeoutMS, TimeUnit.MILLISECONDS);
+
+        // Wait for it to be completed
+        try {
+            handler.get();
+        } catch (CancellationException e) {
+            System.out.println("TIMEOUT!");
+            System.exit(-1);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+    }
+
+    public static void nextInput(){
+        if(current_index++ > 0) {
+            outputs.add(currentOutput);
+        }
+        System.out.printf("next input: for output '%s'\n", currentOutput);
+        currentOutput = "";
+    }
+}
